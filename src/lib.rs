44760f2234a5973c26abbad1c87fd40dcea15959
helbadao <you@example.com>
2025-09-23 18:41:00 +0100

    Update existing files (~2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use sdl2::rect::Point;
+use sdl2::pixels::Color;
+use sdl2::sys::key_t;
+
 pub struct Points(pub Point, pub Point);
 pub fn generate_points(size: (u32, u32)) -> Vec<Points> {
     let mut res: Vec<Points> = Vec::new();
@@ -6,63 +9,199 @@ pub fn generate_points(size: (u32, u32)) -> Vec<Points> {
     let middle_h: i32 = (size.1 / 2) as i32;
     let road_w: i32 = ((size.0 * 12) / 100) as i32;
     let road_h: i32 = ((size.0 * 12) / 100) as i32;
-    res.push(Points(
-        Point::new(middle_w - road_w, 0),
-        Point::new(middle_w - road_w, middle_h - road_h),
-    ));
-    res.push(Points(
-        Point::new(0, middle_h - road_h),
-        Point::new(middle_w - road_w, middle_h - road_h),
-    ));
-    res.push(Points(
-        Point::new(middle_w + road_w, 0),
-        Point::new(middle_w + road_w, middle_h - road_h),
-    ));
-    res.push(Points(
-        Point::new(middle_w + road_w, middle_h - road_h),
-        Point::new(middle_w * 2, middle_h - road_h),
-    ));
-    // // nas lakho
-    res.push(Points(
-        Point::new(0, middle_h + road_h),
-        Point::new(middle_w - road_w, middle_h + road_h),
-    ));
-    res.push(Points(
-        Point::new(middle_w - road_w, middle_h + road_h),
-        Point::new(middle_w - road_w, middle_h * 2),
-    ));
-    res.push(Points(
-        Point::new(middle_w + road_w, middle_h + road_h),
-        Point::new(middle_w * 2, middle_h + road_h),
-    ));
-    res.push(Points(
-        Point::new(middle_w + road_w, middle_h + road_h),
-        Point::new(middle_w + road_w, middle_h * 2),
-    ));
+    res.push(
+        Points(Point::new(middle_w - road_w, 0), Point::new(middle_w - road_w, middle_h - road_h))
+    );
+    res.push(
+        Points(Point::new(0, middle_h - road_h), Point::new(middle_w - road_w, middle_h - road_h))
+    );
+    res.push(
+        Points(Point::new(middle_w + road_w, 0), Point::new(middle_w + road_w, middle_h - road_h))
+    );
+    res.push(
+        Points(
+            Point::new(middle_w + road_w, middle_h - road_h),
+            Point::new(middle_w * 2, middle_h - road_h)
+        )
+    );
+    res.push(
+        Points(Point::new(0, middle_h + road_h), Point::new(middle_w - road_w, middle_h + road_h))
+    );
+    res.push(
+        Points(
+            Point::new(middle_w - road_w, middle_h + road_h),
+            Point::new(middle_w - road_w, middle_h * 2)
+        )
+    );
+    res.push(
+        Points(
+            Point::new(middle_w + road_w, middle_h + road_h),
+            Point::new(middle_w * 2, middle_h + road_h)
+        )
+    );
+    res.push(
+        Points(
+            Point::new(middle_w + road_w, middle_h + road_h),
+            Point::new(middle_w + road_w, middle_h * 2)
+        )
+    );
 
-    res.push(Points(
-        Point::new(middle_w, 0),
-        Point::new(middle_w, middle_h - road_h),
-    ));
-    res.push(Points(
-        Point::new(middle_w, middle_h - road_h),
-        Point::new(middle_w, middle_h - road_h),
-    ));
-     res.push(Points(
-        Point::new(middle_w, middle_h + road_h),
-        Point::new(middle_w, middle_h *2),
-    ));
-
-
-    res.push(Points(
-        Point::new(0, middle_h),
-        Point::new(middle_w - road_w, middle_h),
-    ));
-
-    res.push(Points(
-        Point::new(middle_w + road_w, middle_h),
-        Point::new(middle_w * 2, middle_h),
-    ));
-
+    res.push(Points(Point::new(middle_w, 0), Point::new(middle_w, middle_h - road_h)));
+    res.push(
+        Points(Point::new(middle_w, middle_h - road_h), Point::new(middle_w, middle_h - road_h))
+    );
+    res.push(Points(Point::new(middle_w, middle_h + road_h), Point::new(middle_w, middle_h * 2)));
+    res.push(Points(Point::new(0, middle_h), Point::new(middle_w - road_w, middle_h)));
+    res.push(Points(Point::new(middle_w + road_w, middle_h), Point::new(middle_w * 2, middle_h)));
     res
+}
+
+pub enum Direction {
+    Sud,
+    Nord,
+    West,
+    Ouest,
+}
+pub struct Traficlight {
+    pub direction: Direction,
+    pub color: Color,
+    pub allowed: bool,
+    pub x: i32,
+    pub y: i32,
+    pub width: u32,
+    pub hight: u32,
+}
+impl Traficlight {
+    pub fn new(
+        direction: Direction,
+        color: Color,
+        allowed: bool,
+        x: i32,
+        y: i32,
+        width: u32,
+        hight: u32
+    ) -> Self {
+        Self { direction, color, allowed, x, y, width, hight }
+    }
+}
+pub fn generate_traficlight(size: (u32, u32)) -> Vec<Traficlight> {
+    let mut res = Vec::new();
+    let middle_w: i32 = (size.0 / 2) as i32;
+    let middle_h: i32 = (size.1 / 2) as i32;
+    let road_w: i32 = ((size.0 * 12) / 100) as i32;
+    let road_h: i32 = ((size.0 * 12) / 100) as i32;
+    let width = (road_w / 2) as u32;
+    let height = (road_h / 2) as u32;
+    res.push(
+        Traficlight::new(
+            Direction::Nord,
+            Color::RGB(200, 0, 0),
+            false,
+            middle_w - road_w - (width as i32) - 2,
+            middle_h - road_h - (height as i32) - 1,
+            width,
+            height
+        )
+    );
+    res.push(
+        Traficlight::new(
+            Direction::Sud,
+            Color::RGB(200, 0, 0),
+            false,
+            middle_w + road_w + 2,
+            middle_h + road_h + 2,
+            width,
+            height
+        )
+    );
+    res.push(
+        Traficlight::new(
+            Direction::West,
+            Color::RGB(200, 0, 0),
+            false,
+            middle_w + road_w + 3,
+            middle_h - road_h - (height as i32) - 2,
+            width,
+            height
+        )
+    );
+    res.push(
+        Traficlight::new(
+            Direction::Sud,
+            Color::RGB(200, 0, 0),
+            false,
+            middle_w - road_w - (width as i32) - 2,
+            middle_h + road_h + 2,
+            width,
+            height
+        )
+    );
+    res
+}
+pub enum Key {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+pub struct Car {
+    pub dir: Direction,
+    pub color: Color,
+    pub x: i32,
+    pub y: i32,
+    pub width: u32,
+    pub hight: u32,
+}
+impl Car {
+    pub fn new(dir: Direction, color: Color, x: i32, y: i32, width: u32, hight: u32) -> Self {
+        Self { dir, color, x, y, width, hight }
+    }
+}
+
+pub fn generate_cars(key_t: Key, size: (u32, u32)) -> Car {
+    let middle_w: i32 = (size.0 / 2) as i32;
+    let middle_h: i32 = (size.1 / 2) as i32;
+    let road_w: i32 = ((size.0 * 12) / 100) as i32;
+    let road_h: i32 = ((size.1 * 12) / 100) as i32;
+    let width = (road_w / 2) as u32;
+    let height = (road_h / 2) as u32;
+
+    match key_t {
+        Key::Up =>
+            Car::new(
+                Direction::Sud,
+                Color::RGB(200, 0, 0),
+                middle_w + (width as i32) / 2,
+                (size.1 as i32) - (height as i32),
+                width,
+                height
+            ),
+        Key::Left =>
+            Car::new(
+                Direction::West,
+                Color::RGB(200, 0, 0),
+                (size.0 as i32) - (width as i32),
+                middle_h - (height as i32) / 2 - (height as i32),
+                width,
+                height
+            ),
+        Key::Down =>
+            Car::new(
+                Direction::Nord,
+                Color::RGB(200, 0, 0),
+                middle_w - (width as i32) / 2 - (height as i32),
+                0,
+                width,
+                height
+            ),
+        _ =>
+            Car::new(
+                Direction::Ouest,
+                Color::RGB(200, 0, 0),
+                0,
+                middle_h + (height as i32)/2,
+                width,
+                height
+            ),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,31 @@ use road_intersection::*;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use std::time::Duration;
+
+pub struct Sud {}
+pub struct Nord {}
+pub struct Est {}
+pub struct Ouest {}
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("rust-sdl2 demo", 600, 600)
+        .window("rust-sdl2 demo", 800, 800)
         .position_centered()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
     let lines = generate_points(canvas.window().size());
+    let lights = generate_traficlight(canvas.window().size());
 
     let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut cars: Vec<Car> = Vec::new();
+
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -27,20 +36,51 @@ pub fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
-                Event::KeyDown {keycode:Some(Keycode:: Up),..}=>{},
+
+                Event::KeyDown {
+                    keycode: Some(Keycode::Up),
+                    ..
+                } => cars.push(generate_cars(Key::Up, canvas.window().size())),
+
+                Event::KeyDown {
+                    keycode: Some(Keycode::Down),
+                    ..
+                } => cars.push(generate_cars(Key::Down, canvas.window().size())),
+
+                Event::KeyDown {
+                    keycode: Some(Keycode::Left),
+                    ..
+                } => cars.push(generate_cars(Key::Left, canvas.window().size())),
+
+                Event::KeyDown {
+                    keycode: Some(Keycode::Right),
+                    ..
+                } => cars.push(generate_cars(Key::Right, canvas.window().size())),
+
                 _ => {}
             }
         }
 
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
-        canvas.set_draw_color(Color::RGB(255, 255, 255));
 
         for (h, i) in lines.iter().enumerate() {
             if h > 7 {
                 canvas.set_draw_color(Color::RGB(128, 128, 128));
+            } else {
+                canvas.set_draw_color(Color::RGB(255, 255, 255));
             }
             let _ = canvas.draw_line(i.0, i.1);
+        }
+
+        for light in lights.iter() {
+            canvas.set_draw_color(light.color);
+            let _ = canvas.draw_rect(Rect::new(light.x, light.y, light.width, light.hight));
+        }
+
+        for car in cars.iter() {
+            canvas.set_draw_color(car.color);
+            let _ = canvas.draw_rect(Rect::new(car.x, car.y, car.width, car.hight));
         }
 
         canvas.present();
