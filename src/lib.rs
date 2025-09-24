0351dc5276d69a5f48cefe9146a4476ec907e443
Your Name <you@example.com>
2025-09-24 23:36:22 +0100

    Update existing files (~1)

--- src/lib.rs
+++ src/lib.rs
@@ -213,7 +213,8 @@ pub fn generate_cars(key_t: Key, size: (u32, u32)) -> Car {
         Color::RGB(0, 0, 255),
     ];
 
-    let  g = rand::Rng::gen_range(0..=2);
+    let mut rng = rand::thread_rng();
+    let  g = rng.gen_range(0..=2);
 
     match key_t {
         Key::Up => Car::new(
